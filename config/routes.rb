39bba47cf4b60309c4c6a45a118397424d5c5a66
taji <taji@sonicgarden.jp>
2025-03-12 21:26:39 +0900

    instructorsルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :admins do
     root 'home#index'
+    resources :instructors, only: %i[index new create edit update destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
