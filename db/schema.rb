8720ac8df74dfa55f2fc3001dc77d6d17ebec9f8
taji <taji@sonicgarden.jp>
2025-03-12 18:50:27 +0900

    instructorsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_223938) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_12_094628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,5 +26,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_223938) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_instructors_on_email", unique: true
   end
 end
