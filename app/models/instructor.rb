34777245f39d70aeddd4815441959e8bee95a218
taji <taji@sonicgarden.jp>
2025-03-13 08:22:55 +0900

    instructorにlessonのリレーションを追加

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,5 +2,7 @@ class Instructor < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  has_many :lessons, dependent: :destroy
+
   scope :default_order, -> { order(:id) }
 end
