0340f320b4d3e2ceaed4a6151153999c11914d72
taji <taji@sonicgarden.jp>
2025-03-16 20:36:02 +0900

    lesson_datesテーブルマイグレ

--- db/migrate/20250316113432_create_lesson_questions.rb
+++ db/migrate/20250316113432_create_lesson_questions.rb
@@ -2,7 +2,7 @@ class CreateLessonQuestions < ActiveRecord::Migration[8.0]
   def change
     create_table :lesson_questions do |t|
       t.references :lesson, null: false, foreign_key: true
-      t.string :content
+      t.string :content, null: false
 
       t.timestamps
     end
