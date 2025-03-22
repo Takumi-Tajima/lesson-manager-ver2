803f6e015e4a11d137083d7dbe01ed868a55e5b7
taji <taji@sonicgarden.jp>
2025-03-22 13:44:16 +0900

    lesson_question_answersのfactroyを定義

--- spec/factories/lesson_question_answers.rb
+++ spec/factories/lesson_question_answers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :lesson_question_answer do
-    reservation_id { nil }
-    question { "MyString" }
-    answer { "MyString" }
+    reservation
+    question
+    answer
   end
 end
