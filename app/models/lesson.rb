a685ad96b0d1789bd5a4ab0ea68a95d0595b7e27
taji <taji@sonicgarden.jp>
2025-03-27 22:30:26 +0900

    レッスンの説明がない場合は、後悔できない

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,10 +2,19 @@ class Lesson < ApplicationRecord
   include Publishable
 
   validates :name, presence: true
+  validate :must_have_description_when_published
 
   belongs_to :instructor
   has_many :lesson_dates, dependent: :destroy
   has_many :lesson_questions, dependent: :destroy
 
   scope :default_order, -> { order(:id) }
+
+  private
+
+  def must_have_description_when_published
+    if published.present? && description.blank?
+      errors.add(:published, :must_have_description_when_published)
+    end
+  end
 end
