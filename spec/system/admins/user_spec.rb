67b24c1d643abb6c1b5baae44065f5e798c65608
taji <taji@sonicgarden.jp>
2025-04-06 23:37:56 +0900

    詳細画面のスペックちか

--- spec/system/admins/user_spec.rb
+++ spec/system/admins/user_spec.rb
@@ -4,24 +4,32 @@ RSpec.describe 'ユーザーの機能', type: :system do
   let(:admin) { create(:admin) }
 
   before do
+    travel_to '2025-04-06 12:00:00'
     sign_in admin
-    create(:user, name: '田島匠', email: 'tajitaji@exmaple.com')
+    create(:user, id: 30, name: '田島匠', email: 'tajitaji@exmaple.com', last_sign_in_at: '2025-04-05 12:00:00', created_at: '2025-03-17 13:23:00')
     create(:user, name: '本田圭佑', email: 'honda@exmaple.com')
   end
 
-  describe 'ユーザー一覧機能' do
-    it 'ユーザーの一覧データを閲覧できること' do
+  describe 'ユーザー情報のデータ表示' do
+    it 'ユーザーの情報を閲覧できること' do
       visit admins_users_path
 
       expect(page).to have_selector 'h1', text: 'ユーザー一覧'
 
-      expect(page).to have_content 'ID'
       expect(page).to have_content 'ユーザー名'
       expect(page).to have_content 'メールアドレス'
       expect(page).to have_content '田島匠'
       expect(page).to have_content 'tajitaji@exmaple.com'
       expect(page).to have_content '本田圭佑'
       expect(page).to have_content 'honda@exmaple.com'
+
+      click_on '田島匠'
+
+      expect(page).to have_selector 'h1', text: '田島匠'
+      expect(page).to have_content 'メールアドレス: tajitaji@exmaple.com'
+      expect(page).to have_content '最終ログイン日時: 2025年04月05日(土) 12時00分'
+      expect(page).to have_content '登録日時: 2025年03月17日(月) 13時23分'
+      expect(page).to have_content '更新日時: 2025年04月06日(日) 12時00分'
     end
   end
 end
