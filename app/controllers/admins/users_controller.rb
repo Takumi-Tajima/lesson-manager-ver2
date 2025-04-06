f654a86f123c19a98d3667e79b8c519909f9fbc0
taji <taji@sonicgarden.jp>
2025-04-06 23:37:18 +0900

    詳細画面追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,5 +1,16 @@
 class Admins::UsersController < Admins::ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.default_order
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params.expect(:id))
   end
 end
